7b637e3de04941c5acab470991ece32e3a59fed9
Julieta M <julietamendieta1364@gmail.com>
2023-11-07 10:26:38 -0300

    Modificando detalles

--- categorias_libros/forms.py
+++ categorias_libros/forms.py
@@ -5,34 +5,34 @@ class FormularioFiccion(forms.Form):
     nombre = forms.CharField(required=True, max_length=256)
     autor = forms.CharField(required=True, max_length=256)
     año_publicacion = forms.IntegerField(required=True, max_value=3000)
-    sinopsis = forms.CharField(required=True, max_length=400)
-    reseña = forms.CharField(required=True, max_length=400)
+    sinopsis = forms.CharField(required=True, max_length=3000)
+    reseña = forms.CharField(required=False, max_length=3000)
 
 class FormularioPoesia(forms.Form):
     nombre = forms.CharField(required=True, max_length=256)
     autor = forms.CharField(required=True, max_length=256)
     año_publicacion = forms.IntegerField(required=True, max_value=3000)
-    sinopsis = forms.CharField(required=True, max_length=400)
-    reseña = forms.CharField(required=True, max_length=400)
+    sinopsis = forms.CharField(required=True, max_length=3000)
+    reseña = forms.CharField(required=False, max_length=3000)
 
 class FormularioInfantilYJuvenil(forms.Form):
     genero = forms.CharField(required=True, max_length=256)
     nombre = forms.CharField(required=True, max_length=256)
     autor = forms.CharField(required=True, max_length=256)
     año_publicacion = forms.IntegerField(required=True, max_value=3000)
-    sinopsis = forms.CharField(required=True, max_length=400)
-    reseña = forms.CharField(required=True, max_length=400)
+    sinopsis = forms.CharField(required=True, max_length=3000)
+    reseña = forms.CharField(required=False, max_length=3000)
 
 class FormularioBiografia(forms.Form):
     nombre = forms.CharField(required=True, max_length=256)
     autor = forms.CharField(required=True, max_length=256)
     año_publicacion = forms.IntegerField(required=True, max_value=3000)
-    sinopsis = forms.CharField(required=True, max_length=400)
-    reseña = forms.CharField(required=True, max_length=400)
+    sinopsis = forms.CharField(required=True, max_length=3000)
+    reseña = forms.CharField(required=False, max_length=3000)
 
 class FormularioFilosofiaYReligion(forms.Form):
     nombre = forms.CharField(required=True, max_length=256)
     autor = forms.CharField(required=True, max_length=256)
     año_publicacion = forms.IntegerField(required=True, max_value=3000)
-    descripcion = forms.CharField(required=True, max_length=400)
-    reseña = forms.CharField(required=True, max_length=400)
+    descripcion = forms.CharField(required=True, max_length=3000)
+    reseña = forms.CharField(required=False, max_length=3000)
