9c9259aa668b387d7164262d18042ee73e93fdca
Julieta M <julietamendieta1364@gmail.com>
2023-11-16 23:28:43 -0300

    Mostrar avatar

--- perfiles/forms.py
+++ perfiles/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from perfiles.models import Avatar
 
 class UserRegisterForm(UserCreationForm):
    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
@@ -15,3 +16,9 @@ class UserUpdateForm(forms.ModelForm):
    class Meta:
        model = User
        fields = ['last_name', 'first_name', 'email']
+
+class AvatarFormulario(forms.ModelForm):
+
+   class Meta:
+       model = Avatar
+       fields = ['imagen']
--- perfiles/urls.py
+++ perfiles/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', login_view, name="login"),
     path('logout/', CustomLogoutView.as_view(), name="logout"),
     path('editar-mi-perfil/', MiPerfilUpdateView.as_view(), name="editar_perfil"),
+    path('agregar-avatar/', agregar_avatar, name="agregar_avatar"),
 ]
